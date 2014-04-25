5b7f961599fc898ae596ca8db1d3dce9505c5e32
Martin Coulthurst <mc@lightness.co.uk>
2014-04-25 11:40:06 +0100

    Changed site to use menu option2a

--- js/alpha.js
+++ js/alpha.js
@@ -2,119 +2,20 @@
 
 $(document).ready(function() {
 
-  // catch errors with closing menu
-  $(document).on('click', '.yamm .dropdown-menu', function(e) {  
-    e.stopPropagation()  
-  })  
+      // nav bar menu 
+      $('.collapse').on('shown.bs.collapse', function (e) {
+        $('.collapse').not(this).removeClass('in');
+      });
+
+      $('[data-toggle=collapse]').click(function (e) {
+
+          //console.log(this);
+          console.log($(this).data('target') );
+
+        $('[data-toggle=collapse]').parent('li').removeClass('active');
+        $(this).parent('li').toggleClass('active');
+      });
 
 
 
-
-	    $('.sparkline').each(function() {
-	    	var wd = $(this).parent().width()-10;
-	    	ht = $(this).parent().height();
-
-        $(this).highcharts({
-
-          chart: {
-            backgroundColor: '#FFF',
-            borderWidth: 0,
-            borderRadius: 0,
-            type: 'area',
-            margin: [2, 0, 2, 0],
-            width: wd,
-            height: ht,
-            skipClone: true,
-            marginTop: 10,
-            style: {
-                overflow: 'visible'
-            }
-          },
-
-          title: {
-            text: ''
-          },
-
-          credits: {
-              enabled: false
-          },
-
-          xAxis: {
-            labels: {
-                enabled: false
-            },
-            title: {
-                text: null
-            },
-            startOnTick: false,
-            endOnTick: false,
-            tickPositions: []
-          },
-
-          yAxis: {
-            minPadding: 0,
-            endOnTick: false,
-            startOnTick: false,
-            labels: {
-              enabled: false
-            },
-            title: {
-              text: null
-            },
-            tickPositions: []
-          },
-
-          legend: {
-              enabled: false
-          },
-
-          tooltip: {
-              backgroundColor: null,
-              borderWidth: 0,
-              shadow: false,
-              useHTML: true,
-              hideDelay: 0,
-              shared: true,
-              padding: 0,
-              positioner: function (w, h, point) {
-                return { x: point.plotX - w / 2, y: point.plotY - h +50};
-              },
-            headerFormat: '',
-            pointFormat: '{point.y}'
-          },
-
-          plotOptions: {
-            series: {
-              animation: false,
-              lineWidth: 1,
-              shadow: false,
-              states: {
-                hover: {
-                  lineWidth: 1
-                }
-              },
-
-              marker: {
-                radius: 0,
-                states: {
-                  hover: {
-                    radius: 2
-                  }
-                }
-              },
-
-              fillOpacity: 0.25
-            }
-          },
-
-          series: [{
-              data: $(this).data('sparkline-data')
-          }]
-
-        }); // end highcharts function
-
-      }); // end of .each sparkline
-        
-
-        $(".ons-chart").onsc();
 });
