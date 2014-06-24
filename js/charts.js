29f2e630b10298ddad2c12ad06c45ca3e91c93bb
Martin Coulthurst <mcoulthurst@users.noreply.github.com>
2014-06-24 09:39:08 +0100

    Added population pyramid

--- js/charts.js
+++ js/charts.js
@@ -6,6 +6,8 @@ $(document).ready(
 		initMulti();
 
 		showHighcharts();
+
+		populationPyramid();
 	}
 
 	);
@@ -473,5 +475,97 @@ $('#chart_prices').highcharts({
 
 
 }
+
+function populationPyramid(){
+
+    var chart,
+        categories = ['0-4', '5-9', '10-14', '15-19',
+            '20-24', '25-29', '30-34', '35-39', '40-44',
+            '45-49', '50-54', '55-59', '60-64', '65-69',
+            '70-74', '75-79', '80-84', '85-89', '90-94',
+            '95-99', '100 +'];
+    $(document).ready(function() {
+        $('#pyramid').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Population pyramid for England, midyear 2012'
+            },
+            subtitle: {
+                text: 'Source: www.ons.gov.uk'
+            },
+            xAxis: [{
+                categories: categories,
+                reversed: false,
+                labels: {
+                    step: 1
+                }
+            }, { // mirror axis on right side
+                opposite: true,
+                reversed: false,
+                categories: categories,
+                linkedTo: 0,
+                labels: {
+                    step: 1
+                }
+            }],
+            yAxis: {
+                title: {
+                    text: null
+                },
+                labels: {
+                    formatter: function(){
+                        return (Math.abs(this.value) / 1000000) + 'M';
+                    }
+                },
+                min: -4000000,
+                max: 4000000
+            },
+    
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+    
+            tooltip: {
+                formatter: function(){
+                    return '<b>'+ this.series.name +', age '+ this.point.category +'</b><br/>'+
+                        'Population: '+ Highcharts.numberFormat(Math.abs(this.point.y), 0);
+                }
+            },
+   
+            series: [{
+                name: 'Male',
+                data: [-1746181, -1884428, -2089758, -2222362, -2537431, -2507081, -2443179,
+                    -2664537, -3556505, -3680231, -3143062, -2721122, -2229181, -2227768,
+                    -2176300, -1329968, -836804, -354784, -90569, -28367, -3878]
+            }, {
+                name: 'Female',
+                data: [1656154, 1787564, 1981671, 2108575, 2403438, 2366003, 2301402, 2519874,
+                    3360596, 3493473, 3050775, 2759560, 2304444, 2426504, 2568938, 1785638,
+                    1447162, 1005011, 330870, 130632, 21208]
+            }]
+
+/*            series: [{
+                name: 'Male',
+                data: [-1951153, -1776769, -1745043, -1913324, -2140693, -2165760, -2130739,
+                    -2027676, -2313633, -2372899, -2137943, -1864375, -1848213, -1719021,
+                    -1305796, -1118131, -896135, -590364, -372291]
+            }, {
+                name: 'Female',
+                data: [2045247, 1864230, 1831322, 2013226, 2403438, 2191539, 2152085, 2109169,
+                    2008678, 2253651, 232799, 2097878, 1819823, 1776207, 1626032, 1170152,
+                    928768, 637528, 335580, 141158]
+            }]
+*/
+
+        });
+    });
+    
+
+}
+
 
 
