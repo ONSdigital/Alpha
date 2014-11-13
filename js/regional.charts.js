f6a2e199a76a867e47a188974528f32c0433a9dc
Martin Coulthurst <mcoulthurst@users.noreply.github.com>
2014-11-13 15:21:01 +0000

    Regional Page: set trend minimum to zero

--- js/regional.charts.js
+++ js/regional.charts.js
@@ -342,6 +342,8 @@ var options = {
     options.chart.plotBackgroundColor = "#fff";
     options.title ='Population Trend';
     options.xAxis.categories = ['2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014'];
+
+    options.yAxis.min = 0;
     options.xAxis.labels = {
     
       formatter : function() {
